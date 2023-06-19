4a3087656600e7c1ea8e7cf92d302cc4bac05bdc
Test Project <dilushansulakshitha@gmail.com>
2023-06-19 18:25:56 +0530

    test 1

---
+++ src/app/modules/console/console.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConsoleRoutingModule } from './console-routing.module';
+import { ConsoleComponent } from './console.component';
+import { DashboardContextComponent } from './inner-items/dashboard-context/dashboard-context.component';
+
+import { ManageOrderComponent } from './inner-items/dashboard-context/order/manage-order/manage-order.component';
+import { AddOrderComponent } from './inner-items/dashboard-context/order/manage-order/add-order/add-order.component';
+
+import {MatSelectModule} from "@angular/material/select";
+import {NgxMatSelectSearchModule} from "ngx-mat-select-search";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+
+
+
+@NgModule({
+  declarations: [
+    ConsoleComponent,
+    DashboardContextComponent,
+
+    ManageOrderComponent,
+    AddOrderComponent
+  ],
+  imports: [
+    CommonModule,
+    ConsoleRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    NgxMatSelectSearchModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+
+
+  ]
+})
+export class ConsoleModule { }
